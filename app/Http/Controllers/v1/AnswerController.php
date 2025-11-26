4cf9e32a87c95d388a149cdc94183892553f4cd0
Reuvin <reuvinhernandez22@gmail.com>
2025-11-26 21:03:48 +0800

    ADded yml

--- app/Http/Controllers/v1/AnswerController.php
+++ app/Http/Controllers/v1/AnswerController.php
@@ -16,7 +16,7 @@ class AnswerController extends Controller
     /**
      * Display a listing of the resource.
      */
-    //TEST CODE
+    //TEST CODE Code
     public function index()
     {
         $answers = Answer::all();
