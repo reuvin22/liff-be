5904be1691664137e112516b1129a3f2b47bb47d
Reuvin <reuvinhernandez22@gmail.com>
2025-11-26 20:59:28 +0800

    Update

--- app/Http/Controllers/v1/AnswerController.php
+++ app/Http/Controllers/v1/AnswerController.php
@@ -16,7 +16,7 @@ class AnswerController extends Controller
     /**
      * Display a listing of the resource.
      */
-    //TEST
+    //TEST CODE
     public function index()
     {
         $answers = Answer::all();
